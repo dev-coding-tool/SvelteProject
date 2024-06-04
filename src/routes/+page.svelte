1ab0e14297b0662444290f390f8f70963680730a
jinhyuk <kjh456879@naver.com>
2024-06-04 22:17:43 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	/>
+</svelte:head>
+<div>
+	<ul>
+		<li><a href="/">HOME</a></li>
+		<li><a href="/brand">BRAND</a></li>
+	</ul>
+</div>
+
+<style>
+	@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+</style>

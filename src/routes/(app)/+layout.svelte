c1323b8b97a7e954067048b8bfe5a244ea621aa9
jinhyuk <kjh456879@naver.com>
2024-06-11 00:52:35 +0900

    css import layout.svelte로 이동

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navigation from '~/component/Navigation/Navigation.svelte';
 	import Footer from '~/component/Footer/Footer.svelte';
+	import '../../lib/css/_common.css';
 </script>
 
 <Navigation/>

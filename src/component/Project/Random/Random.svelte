7628fa24e72838a18178eaf904eb24611bb1d70f
jinhyuk <kjh456879@naver.com>
2024-07-06 02:50:17 +0900

    build test

--- src/component/Project/Random/Random.svelte
+++ src/component/Project/Random/Random.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="border border-danger" style="height: 500px;">
-	<h1>Category</h1>
+	<h1>Category</h1> 
 	<button on:click={restart}>Get Category</button>
     {#each $todos as item}
         <p>{item.title}</p>

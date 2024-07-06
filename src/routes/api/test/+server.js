d4ec285f384094d495108f3b82e02d1be9cbb571
jinhyuk <kjh456879@naver.com>
2024-07-06 18:27:25 +0900

    Revert "require 제거"
    This reverts commit 2d3160dabcdc16c57a46d3777ac2f0453ef02039.

--- src/routes/api/test/+server.js
+++ src/routes/api/test/+server.js
@@ -1,6 +1,7 @@
 import { json } from '@sveltejs/kit';
 import dotenv from "dotenv";
 dotenv.config();
+const puppeteer = require('puppeteer-core')
 import * as puppeteer from 'puppeteer-core';
 
 let categoryList = [];
